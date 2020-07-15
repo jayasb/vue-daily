92607d96e270b38016c4596884581e2d5398d7a5
Jayasb <jayasb@gmail.com>
2020-07-15 15:13:47 -0400

    day 12 related commit

--- bvue/src/views/MyTable.vue
+++ bvue/src/views/MyTable.vue
@@ -1,40 +1,45 @@
 <template>
-  <div>
-    <b-table
-      :items="myProvider"
-      :fields="fields"
-    ></b-table>
-  </div>
+    <div>
+        <b-table
+                selectable
+                select-mode="multi"
+                :items="myProvider"
+                :fields="fields"
+                @row-selected="onRowSelected"></b-table>
+    </div>
 </template>
 
 <script>
-  import axios from 'axios';
+    import axios from 'axios';
 
-  export default {
-    data() {
-      return {
-        sortBy: 'age',
-        sortDesc: false,
-        fields: [
-          { key: 'id', sortable: true },
-          { key: 'last_name', sortable: true },
-          { key: 'first_name', sortable: true },
-          { key: 'email', sortable: true }
-        ]
-      }
-    },
-    methods: {
-      myProvider () {
-        let promise = axios.get('https://reqres.in/api/users')
+    export default {
+        data() {
+            return {
+                sortBy: 'age',
+                sortDesc: false,
+                fields: [
+                    {key: 'id', sortable: true},
+                    {key: 'last_name', sortable: true},
+                    {key: 'first_name', sortable: true},
+                    {key: 'email', sortable: true}
+                ]
+            }
+        },
+        methods: {
+            myProvider() {
+                let promise = axios.get('https://reqres.in/api/users')
 
-        return promise.then((response) => {
-          this.items = response.data.data
-          return(this.items)
-        }).catch(error => {
-          console(error)
-          return []
-        })
-      }
+                return promise.then((response) => {
+                    this.items = response.data.data
+                    return (this.items)
+                }).catch(error => {
+                    console(error)
+                    return []
+                })
+            },
+            onRowSelected(items) {
+                console.log(items)
+            }
+        }
     }
-  }
 </script>
---
+++ day 12/readme.txt
@@ -0,0 +1,15 @@
+Day 12
+
+Today, we are going to add row selection feature
+
+For more details -
+https://bootstrap-vue.org/docs/components/table
+refer to row select support
+
+copy MyTable.vue to the views folder
+
+Added the following attributes to enable row selection
+selectable
+select-mode="multi"
+
+Event handler onRowSelected provides the row details
